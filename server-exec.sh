de7921052b1f0902201e9741a49cfa16f2784f34
Anas Juwaidi <anas.didi95@gmail.com>
2020-09-06 19:35:35 +0800

    Update script

--- server-exec.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-## Docker Compose file
-DC_MONGO=docker-compose-mongo.yml
-DC_APP=docker-compose-app-security.yml
-
-docker-compose -f $DC_MONGO -f $DC_APP exec $1 $2
--- server-start.sh
+++ server-start.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
-## Docker Compose file
-DC_MONGO=docker-compose-mongo.yml
-DC_APP=docker-compose-app-security.yml
-
-echo $DC_APP
-
 ## Command line arguments
 IS_DISABLE_MONGO_EXPRESS=${1:-false}
-echo IS_DISABLE_MONGO_EXPRESS = ${IS_DISABLE_MONGO_EXPRESS}
+
+## Print command line arguments
+echo / Command line argument
+echo 1: IS_DISABLE_MONGO_EXPRESS=$IS_DISABLE_MONGO_EXPRESS
+echo
 
 ## Run Process
-docker-compose -f $DC_MONGO -f $DC_APP down
-docker-compose -f $DC_MONGO -f $DC_APP build
-docker-compose -f $DC_MONGO -f $DC_APP up -d
+docker-compose down
+docker-compose up -d
 
 if ${IS_DISABLE_MONGO_EXPRESS}; then
-    docker-compose -f $DC_MONGO -f $DC_APP stop mongo-express
+    docker-compose stop mongo-express
 fi
