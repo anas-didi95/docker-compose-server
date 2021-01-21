8fce18cbb631de18a69fc1364b26b77c9168a7b1
Anas Juwaidi <anas.didi95@gmail.com>
2021-01-21 20:25:22 +0800

    Amend script to display message after server started

--- server-start.sh
+++ server-start.sh
@@ -4,15 +4,22 @@
 IS_PRODUCTION=${1:-false}
 
 ## Print command line arguments
-echo / Command line argument
-echo 1: IS_PRODUCTION=$IS_PRODUCTION
+echo "/ Command line argument"
+echo "1: IS_PRODUCTION=$IS_PRODUCTION"
 echo
 
 ## Run Process
 docker-compose down
 docker-compose up -d
+echo
+echo
 
+echo "##############################################"
 if ${IS_PRODUCTION}; then
+  echo "###   Server started in production mode.   ###"
+else
+  echo "###   Server started in development mode.  ###"
 fi
+echo "##############################################"
 
 docker-compose ps
